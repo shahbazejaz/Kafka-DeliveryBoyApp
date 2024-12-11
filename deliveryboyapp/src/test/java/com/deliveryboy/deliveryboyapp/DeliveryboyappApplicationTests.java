b1a30239c0d17cd7aaadee36b7695cfc69402646
Shahbazejaz <shahbazejaz098@gmail.com>
2024-12-11 13:51:19 +0530

    Initial files

---
+++ deliveryboyapp/src/test/java/com/deliveryboy/deliveryboyapp/DeliveryboyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryboy.deliveryboyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryboyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
